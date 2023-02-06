387659c86d4bd46718e1fc3120f12daf5c105411
nash <103861343+nashgabs@users.noreply.github.com>
2023-02-06 13:52:47 +0000

    2.1.2.2 fixed.

--- tasks/semester-b/2.1.2.2.cpp
+++ tasks/semester-b/2.1.2.2.cpp
@@ -1,86 +1,79 @@
 #include <iostream>
-#include <cstdlib>
-#include <chrono>
+#include <cmath>
+
+long long RUN_MAX = pow(10,6); // 1 million.
+int drawn[6]; // declaration of array to hold the lucky draw.
+bool searchval(int x, int *arr); // pre-define function to search a 1 X 6 array for a single value.
+void draw();
 
 using namespace std;
-
-int *draw();                     // pre-define function to draw numbers into array of size 6
-bool searchval(int x, int *arr); // pre-define function to search a 1 X 6 array for a single value
 int main()
 {
-    int i; // declaration of counter
-    int j; // declaration of counter
+    long long i; // declaration of counter.
+    long long mtch; // integer to store number of matches.
+    int j; // declaration of counter.
+    int picked[6]; // declaration of array to hold chosen numbers
+    short int lim; // integer to store number of matches required.
+    short int n; // integer to store number of individually matched numbers.
+    i = mtch = j = lim = n = 0; // intitialise all to zero.
 
-    int *ptr;    // declaration of pointer to hold address of the lucky draw
-    int pick[6]; // declaration of array to hold chosen numbers
-    int total_matches;
-    int current_matches;
-    int n = 1;
-    bool match = false;
-
-    cout << "Pick your six numbers from 1 to 50 seperated by a space" << endl;
-    for (i = 0; i < 6; i++) // pick lotto numbers
+    srand(time(NULL));
+    std::cout << "Pick your six numbers from 1 to 50 seperated by a space" << endl;
+    for (i = 0; i < 6; i++) // pick lotto numbers.
     {
-        cin >> pick[i];
+        cin >> picked[i];
     }
-    ptr = draw();
-    cout << "How many matching numbers required?" << endl;
-    cin >> total_matches;
 
-    while (current_matches != total_matches) // repeat until the lucky draw contains total_matches
+    cout << "How many matching numbers required?" << endl;
+    cin >> lim;
+
+
+    for (i = 0; i < RUN_MAX; i++) // for 1 million.
     {
-        n++;
-        current_matches = 0;
-        for (i = 0; i < 6; i++)
+        n = 0;
+        draw(); // draw the lottery.
+        for (j = 0; j < 6; j++) // for each element in picked[].
         {
-            if (searchval(pick[i], ptr))
+            if (searchval(picked[j], drawn)) // is the number a winner?
             {
-                // cout << ptr[0] << "\t" << ptr[1] << "\t" << ptr[2] << "\t" << ptr[3] << "\t" << ptr[4] << "\t" << ptr[5] << endl;
-                // cout << pick[i] << endl;
-                current_matches++;
-                // if (current_matches == total_matches)
-                // {
-                //     goto escape;
-                // }
+                n++; // add a win.
             }
         }
-        ptr = draw();
+        if (n >= lim) // does it meet the required number of matches?
+        {
+            mtch++;
+        }                        
     }
-    // escape:
-    cout << "Number of trials before " << total_matches << " number(s) had been matched:" << endl;
-    cout << n << endl;
-    cout << "\nLotto numbers:\t";
-    for (i = 0; i < 6; i++)
-    {
-        cout << ptr[i] << "\t";
-    }
+
+    cout << "Number of times " << lim << " matches occured in " << RUN_MAX << " runs:" << endl;
+    cout << mtch << endl;
+
     cout << "\n\nYour pick:\t";
     for (i = 0; i < 6; i++)
     {
-        cout << pick[i] << "\t";
+        cout << picked[i] << "\t";
     }
     return 0;
 }
 
-int *draw()
+void draw() // update array with new lotto draw
 {
-    srand(time(NULL));      // seed the RNG with the current time
-    static int arr[6];      // declaration of static array to store the lucky draw
-    int i;                  // declaration of counter
-    for (i = 0; i < 6; i++) // populate the array
-        arr[i] = (rand() % 50) + 1;
-    return arr; // return the array address
+    int i = 0;
+    for (i = 0; i < 6; i++) // draw lotto numbers into global list drawn[].
+    {
+        drawn[i] = (rand() % 50) + 1;
+    }
 }
 
-bool searchval(int x, int *arr)
+bool searchval(int x, int* arr) // search for value in array.
 {
     int i;
-    for (i = 0; i < 6; i++)
+    for (i = 0; i < 6; i++) // for array elements.
     {
-        if (x == arr[i])
+        if (x == arr[i]) // if value is in array return true, exit.
         {
             return true;
         }
     }
-    return false;
+    return false; // else false, exit.
 }
