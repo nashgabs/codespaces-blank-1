3bd2bbc18fba932c23a3d0ba5ebbdc02280c4b56
nash <103861343+nashgabs@users.noreply.github.com>
2023-02-13 12:11:55 +0000

    2.1.2.2 modified

--- tasks/semester-b/2.1.2.2.cpp
+++ tasks/semester-b/2.1.2.2.cpp
@@ -1,79 +1,91 @@
 #include <iostream>
 #include <cmath>
+#define MAX 10000000 // max runs
+#define N 50         // max lotto number
 
-long long RUN_MAX = pow(10,6); // 1 million.
-int drawn[6]; // declaration of array to hold the lucky draw.
-bool searchval(int x, int *arr); // pre-define function to search a 1 X 6 array for a single value.
-void draw();
+// no duplicate numbers
+// no numbers smaller than 1 or greater than 50
+void draw(int luckyn[6]);
 
 using namespace std;
 int main()
 {
-    long long i; // declaration of counter.
-    long long mtch; // integer to store number of matches.
-    int j; // declaration of counter.
-    int picked[6]; // declaration of array to hold chosen numbers
-    short int lim; // integer to store number of matches required.
-    short int n; // integer to store number of individually matched numbers.
-    i = mtch = j = lim = n = 0; // intitialise all to zero.
+    int _temp, i, j, k;
+    long long ndraw[6];
+    int usern[6], luckyn[6] = {0};
+    bool inuse[N] = {false};
 
-    srand(time(NULL));
-    std::cout << "Pick your six numbers from 1 to 50 seperated by a space" << endl;
-    for (i = 0; i < 6; i++) // pick lotto numbers.
-    {
-        cin >> picked[i];
-    }
+    draw(luckyn);
 
-    cout << "How many matching numbers required?" << endl;
-    cin >> lim;
-
-
-    for (i = 0; i < RUN_MAX; i++) // for 1 million.
-    {
-        n = 0;
-        draw(); // draw the lottery.
-        for (j = 0; j < 6; j++) // for each element in picked[].
-        {
-            if (searchval(picked[j], drawn)) // is the number a winner?
-            {
-                n++; // add a win.
-            }
-        }
-        if (n >= lim) // does it meet the required number of matches?
-        {
-            mtch++;
-        }                        
-    }
-
-    cout << "Number of times " << lim << " matches occured in " << RUN_MAX << " runs:" << endl;
-    cout << mtch << endl;
-
-    cout << "\n\nYour pick:\t";
+    cout << "Enter your six lucky numbers between 1 and 50!" << endl;
     for (i = 0; i < 6; i++)
     {
-        cout << picked[i] << "\t";
+        cout << i + 1 << ":\t";
+        cin >> _temp;
+        while (_temp > 50 || _temp < 1 || inuse[_temp] == true)
+        {
+            cout << "Try again\n"
+                 << i + 1 << ":\t";
+            cin >> _temp;
+        }
+        inuse[_temp] = true;
+        usern[i] = _temp;
     }
+
+    // draw the lucky numbers and count how many matching numbers there are
+    k = 0;
+    _temp = 0;
+    while (_temp < 6 && k < MAX)
+    {
+        draw(luckyn);
+        _temp = 0;
+        for (i = 0; i < 6; i++)
+        {
+            for (j = 0; j < 6; j++)
+            {
+                if (usern[i] == luckyn[j])
+                {
+                    _temp++;
+                }
+            }
+        }
+        if (_temp != 0 && ndraw[_temp] != 0)
+        {
+            for (i = 0; i < _temp - 1; i++)
+            {
+                if (ndraw[i] == 0)
+                {
+                    ndraw[i] = _temp;
+                }
+            }
+        }
+        k++;
+    }
+
+    for (i = 0; i < 6; i++)
+    {
+        cout << i + 1 << " lucky matches took " << ndraw[i] << " lotto draws." << endl;
+    }
+
     return 0;
 }
 
-void draw() // update array with new lotto draw
+void draw(int luckyn[6])
 {
-    int i = 0;
-    for (i = 0; i < 6; i++) // draw lotto numbers into global list drawn[].
+    int pile[N + 1], i, x, _temp;
+    for (i = 1; i < N + 1; i++) // fill the pile with numbers 1 to 50 where pile[1]=1 and pile[50]=50.
     {
-        drawn[i] = (rand() % 50) + 1;
+        pile[i] = i;
     }
-}
-
-bool searchval(int x, int* arr) // search for value in array.
-{
-    int i;
-    for (i = 0; i < 6; i++) // for array elements.
+    for (i = 1; i < N + 1; i++) // shuffle the numbers randomly
     {
-        if (x == arr[i]) // if value is in array return true, exit.
-        {
-            return true;
-        }
+        x = (rand() % 50) + 1; // random number 1 to 50
+        _temp = pile[i];       // preserve the number at current location
+        pile[i] = pile[x];     // i --> x
+        pile[x] = _temp;       // x --> i
     }
-    return false; // else false, exit.
+    for (i = 1; i < 7; i++) // map pile[1...6] onto lucky number list
+    {
+        luckyn[i] = pile[i];
+    }
 }
